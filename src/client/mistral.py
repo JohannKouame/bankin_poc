6574b2a6e80cfb9edeb433eb97e7ed91f24ccde3
johannkouame <kouamremy4@gmail.com>
2026-02-16 22:32:06 +0100

    Fix(LLM): Add categories_jsonRefactor LLM system
    - Add stream option to get model's response progressively
    - Modify prompts to make model more precise
    - Reorganise mistral_repository.py methods
    - Add documentation and comments

--- src/client/mistral.py
+++ src/client/mistral.py
@@ -1,37 +1,75 @@
-import json
-import logging
-import os
-import re
-from src.utils.string_sanitizer import StringSanitizer
+# ./src/client/mistral_client.py
 
+import os
 from mistralai import Mistral
-import requests
+from typing import Generator
 
 LOGGING_VARIABLE = "[MISTRAL]"
+
 class MistralClient:
     def __init__(self) -> None:
         self.token = os.environ["MISTRAL_TOKEN_API"]
         self.default_model = os.environ["MISTRAL_MODEL_NAME"]
 
-    def request(self, prompt: str):
-        with Mistral(
-                api_key=self.token
-        ) as mistral:
+    def request(self, prompt: str) -> str:
+        """
+        Request model in stream mode
+        :param prompt: str
+            user prompt
 
-            res = mistral.chat.complete(model=self.default_model, messages=[
-                {
-                    "content": prompt,
-                    "role": "user",
-                },
-            ], stream=False)
+        :return:
+            model response
+        """
+        with Mistral(api_key=self.token) as mistral:
+            res = mistral.chat.complete(
+                model=self.default_model,
+                messages=[{"role": "user", "content": prompt}],
+                stream=False,
+                max_tokens=1000
+            )
 
-        # Handle response
         return res.model_dump()["choices"][0]["message"]["content"]
 
+    def request_stream(self, prompt: str) -> Generator:
+        """
+        Request model in stream mode
+        :param prompt: str
+            user prompt
 
+        :return: Generator
+            stream response
+        """
+        with Mistral(api_key=self.token) as mistral:
+            stream_response = mistral.chat.stream(
+                model=self.default_model,
+                messages=[{"role": "user", "content": prompt}],
+                max_tokens=1000
+            )
 
-    def chat_completion(self, prompt: str):
-        logging.debug(f"{LOGGING_VARIABLE} Sending request...")
-        response = self.request(prompt)
-        logging.debug(f"{LOGGING_VARIABLE} {response}")
-        return response
+            for chunk in stream_response:
+                delta = chunk.data.choices[0].delta
+
+                if delta.content:
+                    yield delta.content
+
+    def chat_completion(self, prompt: str) -> str:
+        """
+        Method to chat with model
+        :param prompt: str
+            user prompt
+
+        :return: str
+            model answer
+        """
+        return self.request(prompt)
+
+    def chat_completion_stream(self, prompt: str) -> Generator :
+        """
+        Method to chat with model in stream mode
+        :param prompt: str
+            user prompt
+
+        :return: Generator
+            model answer
+        """
+        return self.request_stream(prompt)
--- src/repository/mistral_repository.py
+++ src/repository/mistral_repository.py
@@ -8,87 +8,231 @@ from src.utils.loader import Loader
 class MistralRepository:
     def __init__(self, client: MistralClient) -> None:
         self.client = client
-        self.categories = Loader.json_loader("categories_mapping.json")
+        self.budget_categories = Loader.json_loader("categories_mapping.json")
+        self.prompt_categories = Loader.json_loader("prompt_categories.json")
 
-    def what_is_user_goal(self, message: str) -> dict:
+    def detect_user_prompt_category(self, message: str) -> str:
         """
-        Analyse le texte utilisateur pour extraire l'objectif financier.
+        Determine user prompt's type
         """
-        prompt = f"""Analyse le texte utilisateur et extrais STRICTEMENT les informations suivantes :
-        1) categorie_depense (une seule valeur parmi {list(self.categories.keys())})
-        2) montant_cible (valeur numérique, peut être un montant ou pourcentage)
-        3) horizon (en mois)
-
-        Contraintes :
-        - Réponds UNIQUEMENT en JSON valide
-        - Si information absente → "unprecised"
-
-        Texte utilisateur :
-        {message}
-        """
+        prompt = f"""Analyse le texte de l'utilisateur. L'objectif est de détecter de quel 
+                    type est ce prompt parmi les suivants:
+                    {self.prompt_categories}
+                    Répond avec uniquement la catégorie du prompt, pas le descriptif.
+                    Un prompt ne peut avoir qu'une seule catégorie. Si le prompt est ambigüe, 
+                    réponds avec la catégorie la plus proche 
+            
+                    Texte utilisateur :
+                    {message}
+                    """
 
         response = self.client.chat_completion(prompt)
-        logging.debug(f"{LOGGING_VARIABLE} what_is_user_goal: {response}")
+        logging.debug(f"{LOGGING_VARIABLE} detecte_user_prompt_category: {response}")
         return response
 
-    def propose_optimisation_plan(self, user_goal: dict, stats: dict, message: str) -> str:
+    def detect_target_category(self, message) -> str:
         """
-        Génère un plan ou une analyse synthétique selon les données et l'objectif.
+        Detect categories to focus one for achieve user goal
+        :param message: str
+                user message
+
+        :return:
+            model answer
+        """
+        prompt = f"""Analyse le texte utilisateur et extrais STRICTEMENT les informations suivantes :
+                        1) categoru : UNE SEULE valeur parmi {self.budget_categories.keys()}
+                        2) target_amount : valeur numérique uniquement, peut être explicite (ex: 600) ou % (ex: +20, -15)
+                        3) duration : durée en mois, sans mentionner "mois"
+                        Contraintes :
+                        - Réponds UNIQUEMENT en JSON valide
+                        - Aucun texte hors JSON
+                        - Aucune devise
+                        - Si info absente → "unspecified"
+                        Texte utilisateur : {message}
+                     """
+        response = self.client.chat_completion(prompt)
+        logging.info(f'{LOGGING_VARIABLE} {response}')
+        return response
+
+    def propose_optimisation_plan(self, message: str, stats: dict, stream: bool = True) -> str:
+        """
+        Propose an optimization plan to user
+        :param message:
+            user prompt
+        :param stats: dict
+            contain user transactions
+        :param stream: bool
+            'True' to make model answer on stream
+            'False' to wait until model complete treatment before displaying answer
+
+        :return:
+            model answer
         """
         prompt = f"""
-        Tu es un assistant financier. Les données suivantes sont disponibles :
-        Stats mensuelles: {json.dumps(stats)}
+                Tu es un assistant financier. Données disponibles :
+                Stats mensuelles: {json.dumps(stats)}
+                Message utilisateur : "{message}"
+                Instructions :
+                1) Reste concis et synthétique. Ne mentionne pas toutes les catégories. 
+                    Identifie les plus simple à optimiser (maximum 2 catégories)
+                2) Utilise un tableau si nécessaire
+                3) Donne un résumé clair avant les détails
+                4) Ne pose aucune question
+                5) Ajoute des emojis
+                6) Traduis les catégories en français
+                7) N'excède pas 20 phrases
+                
+                Répond uniquement avec le texte de réponse. Aucun décorateur ni élément autour 
+                de la réponse comme "réponse" ou "voici la réponse"
+                """
 
-        Message utilisateur : "{message}"
-        Objectif utilisateur : {user_goal}
-
-        Instructions :
-        1) Reste concis et synthétique, max 1 ligne par section
-        2) Utilise un tableau si nécessaire
-        3) Donne un résumé clair avant les détails
-        4) Ne pose **aucune question à l'utilisateur**
-        5) Ajoute des emojis pour rendre agréable
-        6) Traduis les catégories en français
-        7) N'excède pas 20 phrases
-        """
-        response = self.client.chat_completion(prompt)
+        if stream:
+            response = self.client.chat_completion(prompt)
+        else:
+            response = self.client.chat_completion_stream(prompt)
         logging.debug(f"{LOGGING_VARIABLE} propose_optimisation_plan: {response}")
+
+        return response
+
+    def answer_to_greeting(self, message: str, stream: bool = True) -> str:
+        """
+        Answer to anny question about greeting
+        :param message:
+            user prompt
+        :param stream: bool
+            'True' to make model answer on stream
+            'False' to wait until model complete treatment before displaying answer
+
+        :return:
+            model answer
+        """
+        prompt = f"""
+                Réponds très brièvement au prompt de l'utilisateur.
+                Répond uniquement avec le texte de réponse. Aucun décorateur ni élément autour 
+                de la réponse comme "réponse" ou "voici la réponse"
+                Ajoute des emojis pour être plus friendly.
+                Texte utilisateur : {message}
+                """
+        if stream:
+            response = self.client.chat_completion(prompt)
+        else:
+            response = self.client.chat_completion_stream(prompt)
+
+        logging.debug(f"{LOGGING_VARIABLE} propose_optimisation_plan: {response}")
+
+        return response
+
+    def answer_to_generality(self, message: str, stream: bool = True) -> str:
+        """
+        Answer to anny question about generality and system presentation
+        :param message:
+            user prompt
+        :param stream: bool
+            'True' to make model answer on stream
+            'False' to wait until model complete treatment before displaying answer
+
+        :return:
+            model answer
+        """
+        prompt = f"""
+                Réponds très brièvement au prompt de l'utilisateur.
+                Répond uniquement avec le texte de réponse. Aucun décorateur ni élément autour 
+                de la réponse comme "réponse" ou "voici la réponse"
+                Ajoute des emojis pour être plus friendly.
+                N'oublie pas que tu es un PoC conçu pour permettre aux utilisateur de simuler l'incidence
+                des décisions et de leurs actions sur leur budget et leurs finances
+                Texte utilisateur : {message}
+                """
+        if stream:
+            response = self.client.chat_completion(prompt)
+        else:
+            response = self.client.chat_completion_stream(prompt)
+
+        logging.debug(f"{LOGGING_VARIABLE} propose_optimisation_plan: {response}")
+
+        return response
+
+    def display_summary(self, message: str, stats: dict, stream: bool = True) -> str:
+        """
+        Answer to any prompt about showing transaction summary
+        :param message:
+            user prompt
+        :param stats: dict
+            contain user transactions
+        :param stream: bool
+            'True' to make model answer on stream
+            'False' to wait until model complete treatment before displaying answer
+
+        :return:
+            model answer
+        """
+
+        prompt = f"""
+                Tu es un assistant financier. Les données suivantes sont disponibles :
+                Stats mensuelles: {json.dumps(stats)}
+                Message utilisateur : "{message}"
+                Instructions :
+                1) Reste concis et synthétique.
+                2) Utilise un tableau si nécessaire
+                3) Donne un résumé clair avant les détails
+                4) Ne pose aucune question
+                5) Ajoute des emojis
+                6) Traduis les catégories en français
+                7) N'excède pas 20 phrases
+                
+                Répond uniquement avec le texte de réponse. Aucun décorateur ni élément autour 
+                de la réponse comme "réponse" ou "voici la réponse"
+                """
+        if stream:
+            response = self.client.chat_completion(prompt)
+        else:
+            response = self.client.chat_completion_stream(prompt)
+
+        logging.debug(f"{LOGGING_VARIABLE} propose_optimisation_plan: {response}")
+
         return response
 
     def chat(self, message: str) -> str:
         """
-        Répond à tout message utilisateur en adaptant la réponse au type de message.
+        To treat any user prompt without preprocessing the prompt
+        :param message: str
+            user prompt
+
+        :return:
+            model answer
         """
-        # Catégorisation simple
-        message_lower = message.lower()
-        salutations = ["hello", "hi", "bonjour", "salut"]
-        analyse_keywords = ["dépense", "budget", "mois", "catégorie", "augmentent", "réduire"]
-        optimisation_keywords = ["économiser", "plan", "objectif", "montant"]
 
-        if any(word in message_lower for word in salutations):
-            return "Bonjour ! Que puis-je faire pour toi ?"
-        elif any(word in message_lower for word in optimisation_keywords + analyse_keywords):
-            # Charger les données pour l'analyse
-            monthly_summary = Loader.csv_loader("data/processed/monthly_summary.csv")
-            sub_categories_stats = Loader.csv_loader("data/processed/sub_categories_stats.csv")
+        response = self.client.chat_completion(message)
+        logging.debug(f"{LOGGING_VARIABLE} chat: {response}")
+        return response
 
-            monthly_summary_dict = monthly_summary.to_dict(orient='records')
-            sub_categories_stats_dict = sub_categories_stats.to_dict(orient='records')
+    def preprocess_and_answer(self, message: str, stats: dict = None, stream: bool = True) -> str:
 
-            prompt = f"""
-            Tu es un assistant financier intelligent.
-            Monthly Summary: {monthly_summary_dict}
-            Sub-category Stats: {sub_categories_stats_dict}
-            Question utilisateur : {message}
+        """
+        Analyse user prompt and detect the financial goal.
+        :param message: str
+            user prompt
+        :param stats: dict
+             user transactions
+        :param stream: bool
+            'True' to make model answer on stream
+            'False' to wait until model complete treatment before displaying answer
 
-            Instructions :
-            - Sois clair et synthétique
-            - Utilise tableaux si nécessaire
-            - Ne pose **aucune question à l'utilisateur**
-            - Traduis les catégories en français
-            """
-            response = self.client.chat_completion(prompt)
-            logging.debug(f"{LOGGING_VARIABLE} chat: {response}")
-            return response
-        else:
-            return "Je n'ai pas compris, peux-tu reformuler ta question ?"
+        :return: str
+            model answer
+        """
+        prompt_category = self.detect_user_prompt_category(message)
+        match prompt_category:
+            case "unspecified":
+                return "Je n'ai pas compris. Pouvez être plus précis svp ?"
+            case "greeting":
+                return self.answer_to_greeting(message=message, stream=stream)
+            case "generality":
+                return self.answer_to_generality(message=message, stream=stream)
+            case "optimization":
+                return self.propose_optimisation_plan(message=message, stats=stats, stream=stream)
+            case "summary":
+                return self.display_summary(message=message, stats=stats, stream=stream)
+            case _:
+                return self.chat(message=message)
+
